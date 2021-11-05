c66b5e9fd8f9e3983edbc43e893d7dcbe5340021
cliffmax85 <cliffmax85@gmail.com>
2021-11-05 09:45:48 -0700

    some new stuff

--- app.js
+++ app.js
@@ -6,3 +6,39 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+const dogSound = document.getElementById('dog-sound');
+const dogImage = document.getElementById('dog-image');
+const catSound = document.getElementById('cat-sound');
+const catImage = document.getElementById('cat-image');
+const horseSound = document.getElementById('horse-sound');
+const horseImage = document.getElementById('horse-image');
+const whichAnimal = document.getElementById('show-animal');
+
+dogImage.addEventListener('click', () => {
+  dogSound.play();
+  whichAnimal.textContent('Doggie!!!');
+});
+
+catImage.addEventListener('click', () => {
+  catSound.play();
+  whichAnimal.textContent('Kitty!!!!');
+});
+
+horseImage.addEventListener('click', () => {
+  horseSound.play();
+  whichAnimal.textContent('Horsey!!!');
+});
+
+// document.addEventListener('keydown', (event) => {
+  
+//   if (event.key === 'c') {
+
+//   }
+
+//   if (event.key === 'd') {
+
+//   }
+
+
+// });
+
